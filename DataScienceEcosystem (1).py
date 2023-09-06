c10fc32a8552186ecea58b9f2e7ba33f52c18571
Froqfunny1 <144225498+Froqfunny1@users.noreply.github.com>
2023-09-06 15:31:40 +0200

    Add files via upload

---
+++ DataScienceEcosystem (1).py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science and Ecosystem
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized
+
+# Some of the popular languages that Data Scientist use are:
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. PHP
+
+# Some of the commonly used libaries used by Data Scientist incluce:
+# * Numpy
+# * Apache Spark
+# * Vegas
+
+# |Data Science Tools|
+# |------------------|
+# |Anaconda|
+# |Jupyter Notebooks|
+# |R-Studio|
+
+# ### Below are few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+(3*4)+5
+
+
+# In[8]:
+
+
+# this will conver 200 minutes to hours by diving by 60
+200/60
+
+
+# ***Objectives:*** 
+# * add comments into code
+# * create markdowns
+# * transfer format of markdowns
+
+# ## Kevin
+# Author
+
+# In[ ]:
+
+
+
+
