a0031dc12592bacdc5eed64b603cceb7a865ec27
Froqfunny1 <144225498+Froqfunny1@users.noreply.github.com>
2023-09-06 14:49:32 +0200

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science and Ecosystem
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized
+
+# Some of the popular languages that Data Scientist use are:
+# 1. language1
+# 2. language2
+# 3. language3
+# 4. language4
+
+# Some of the commonly used libaries used by Data Scientist incluce:
+# * First
+# * Second
+# * Third
+
+# |Data Science Tools|
+# |------------------|
+# |Tool1|
+# |Tool2|
+# |Tool3|
+
+# ### Below are few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+(3*4)+5
+
+
+# In[8]:
+
+
+# this will conver 200 minutes to hours by diving by 60
+200/60
+
+
+# ***Objectives:*** 
+# * First
+# * second
+# * third
+
+# ## Kevin
+# Author
+
+# In[ ]:
+
+
+
+
